69b77fab4f027ef2e690f356140589bddd9df490
Shaun Chow <105621363+shaunchowGO@users.noreply.github.com>
2023-06-04 15:23:48 +1000

    off canvas fix

--- js/main.js
+++ js/main.js
@@ -24,46 +24,14 @@
         }
         sticky_header();
 
-        //  Back to top button
-        $(window).on('scroll', function () {
-            if ($(this).scrollTop() > 600) {
-                $('.back-to-top').fadeIn(200)
-            } else {
-                $('.back-to-top').fadeOut(200)
-            }
-        });
-
-        // animation for Back to top button
-        $('.back-to-top').on('click', function (event) {
-            event.preventDefault();
-
-            $('html, body').animate({
-                scrollTop: 0,
-            }, 1500);
-        });
-
         // Hamburger-menu
         $('.header-left button').on('click', function () {
-            $('.ofcavas-menu').addClass('current');
+            $('.off-canvas-menu').addClass('current');
         });
 
-        $('.offcanvas-bottom span').on('click', function () {
-            $('.ofcavas-menu').removeClass('current');
+        $('.off-canvas-bottom span').on('click', function () {
+            $('.off-canvas-menu').removeClass('current');
         });
-
-        // Hamburger menu
-        var headerLeftBtn = document.querySelector(".header-left button");
-        var ofcanvasMenu = document.querySelector(".ofcavas-menu");
-        var offcanvasBottom = document.querySelector(".offcanvas-bottom span");
-  
-        headerLeftBtn.addEventListener("click", function () {
-         ofcanvasMenu.classList.add("current");
-        });
-  
-        offcanvasBottom.addEventListener("click", function () {
-          ofcanvasMenu.classList.remove("current");
-        });
-
 
         // accordian
         jQuery(".accordian-title").click(function() {
@@ -75,13 +43,7 @@
                 $(this).addClass("active").next().slideDown();
             }
             return false;
-        })
-
-
-
-
-
-        
+        });
     });
 
 })(jQuery);
